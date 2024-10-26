bfda3f9a258088358a533ce89583b4f6fb43f098
Daniel Adekoya <adekoyadaniel53@gmail.com>
2024-10-26 14:40:51 +0100

    "Added framer-motion dependency and updated Home page component with motion and loader elements"

---
+++ src/app/Loader.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Flower from '<absolute>/images/FLOWER.svg'
+import Image from 'next/image';
+import { motion } from "framer-motion";
+
+export default function Loader() {
+    const [loading, setLoading] = useState(true)
+
+    if (typeof window !== "undefined") {
+        window.addEventListener('load', () => {
+            setLoading(false)
+        })
+    }
+
+    return (
+        <motion.div
+           /*  initial={{ transform: "translateY(0%)", opacity: 1 }}
+            whileInView={{ transform: "translateY(0%)", opacity: 0 }}
+            transition={{
+                type: "spring",
+                bounce: 0.2,
+                duration: 0.8,
+                delay: 0.2,
+            }} */ animate={{ opacity: loading ? 1 : 0 }} className={`w-full h-screen fixed top-0 left-0 bg-[#000] grid place-items-center z-10 pointer-events-none`}>
+            <Image src={Flower} className=' animate-spin-slow' alt={''} />
+        </motion.div>
+    );
+}
