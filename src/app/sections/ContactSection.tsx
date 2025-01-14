da49e84b08f685079a30e27d44b3219a8a60afd5
Adekoya Daniel <60848550+dhanielcodes@users.noreply.github.com>
2025-01-14 15:57:04 +0100

    Update ContactSection.tsx
    changed to personal email

--- src/app/sections/ContactSection.tsx
+++ src/app/sections/ContactSection.tsx
@@ -27,7 +27,7 @@ const ContactSection = () => {
   return (
     <a
       target="_blank"
-      href="mailto:adekoyadaniel53@gmail.com"
+      href="mailto:me@dhaniel.site"
       className={`bg-black hover:bg-[#1f1f1f] transition duration-150 ease-linear block  col-span-3  w-full p-[30px] flex justify-between flex-col h-full  rounded-2xl text-white`}
     >
       <div className="flex justify-between">
